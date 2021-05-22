c830ee93f869c128198b3338785440dfcee48a74
SoBoLp <pbolotov@gmail.com>
2021-05-22 18:33:22 +0200

    1392. Longest Happy Prefix https://leetcode.com/problems/longest-happy-prefix/

--- src/com/leetcode/strings/LongestHappyPrefix.java
+++ src/com/leetcode/strings/LongestHappyPrefix.java
@@ -1,11 +1,10 @@
 package com.leetcode.strings;
 
 public class LongestHappyPrefix {
-    public String longestPrefix(String s) {
+    /*public String longestPrefix(String s) {
         String result = "";
         for (int i = s.length() - 1; i > 0; i--) {
             String tmpStr = s.substring(0, i);
-            ;
             if (isSuffix(tmpStr, s)) {
                 result = tmpStr;
                 break;
@@ -16,5 +15,24 @@ public class LongestHappyPrefix {
 
     private boolean isSuffix(String s, String str) {
         return s.equals(str.substring(str.length() - s.length()));
+    }*/
+    public String longestPrefix(String s) {
+        int[] prefixLengths = new int[s.length()];
+        int currentIndex = 1;
+        int lengthOfPrefix = 0;
+        while (currentIndex < s.length()) {
+            char suffixLastChar = s.charAt(currentIndex);
+            char prefixLastChar = s.charAt(lengthOfPrefix);
+            if ( suffixLastChar == prefixLastChar) {
+                prefixLengths[currentIndex] = lengthOfPrefix + 1;
+                currentIndex++;
+                lengthOfPrefix++;
+            } else {
+                if (lengthOfPrefix == 0) {
+                    currentIndex++;
+                } else lengthOfPrefix = prefixLengths[lengthOfPrefix - 1];
+            }
+        }
+        return s.substring(0, prefixLengths[s.length() - 1]);
     }
 }
